9e2d8d2eba43bbea6c7d608748e56abb8e2213b8
entropyconquers <breakingcortana5@gmail.com>
2022-11-06 14:05:40 +0530

    cleanup

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,11 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
 import { Props } from '../types';
-import { Button} from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 export default function TabOneScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Mood')}
